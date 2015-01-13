d3a1a258107fb0740548ca6638196660df9f5d12
PrebenFrenning <preben@pocketoslo.com>
2015-01-13 15:10:07 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<DOCTYPE!html>
+<html>
+<head>
+<title>Such test</title>
+</head>
+<body>
+<?php include('nav.php'); ?>
+
+<h1>Very minimal</h1>
+
+<p>Many commit</p>
+
+<h2>Works very! wow</h2>
+
+<p>Opens great in Github 4 Mac! wow</p>
+
+</body>
+
+</html>
+
